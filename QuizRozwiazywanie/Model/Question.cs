ed34bd7639279d81d453497023248e6ff7967034
karolcieply <72377467+karolcieply@users.noreply.github.com>
2022-04-16 18:48:50 +0200

    Added quiz and question class

---
+++ QuizRozwiazywanie/Model/Question.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizRozwiazywanie.Model
+{
+    internal class Question
+    {
+        public string Content { get; set; }
+        public Dictionary<char, string> Questions { get; set; }
+        public List<char> Answer { get; set; }
+
+        public Question(string content, Dictionary<char, string> questions, List<char> answer)
+        {
+            this.Answer = answer;
+            this.Content = content;
+            this.Questions = questions;
+        }
+
+        public override string ToString()
+        {
+            return $"Answer: {Answer}, Content: {Content}, Questions: {Questions}";
+        }
+
+
+    }
+}
---
+++ QuizRozwiazywanie/Model/Quiz.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.IO;
+
+namespace QuizRozwiazywanie.Model
+{
+    internal class Quiz
+    {
+        public string Name { get; set; }
+        public List<Question> questions;
+        private Question current;
+        private Random random;
+
+        public Quiz(string name, List<Question> questions)
+        {
+            this.Name = name;
+            this.questions = questions;
+            string jsonVar = JsonSerializer.Serialize(this.questions);
+            File.WriteAllText(this.Name + ".json", jsonVar);
+        }
+        public Quiz(string name)
+        {
+            this.Name = name;
+            string jsonString = File.ReadAllText(this.Name + ".json");
+            this.questions = JsonSerializer.Deserialize<List<Question>>(jsonString);
+        }
+        public void NextQuestion()
+        {
+            int rng = this.random.Next(0, this.questions.Count);
+            this.current = this.questions[rng];
+            this.questions.RemoveAt(rng);
+        }
+        public bool CheckAnswer(char answer) => this.current.Answer.Contains(answer);
+        public string getCurrentContent() => this.current.Content;
+        public Dictionary<char, string> getCurrentQuestions() => this.current.Questions;
+    }
+}
